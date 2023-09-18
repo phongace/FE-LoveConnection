eb86f373dfcb867680d0f3fc767342f7b649e53e
Hoang Phong <phongnh2@smartosc.com>
2023-09-18 16:46:39 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StartPage from 'src/screens/StartPage';
+
+function App(): JSX.Element {
+  return <StartPage />;
+}
+
+export default App;
---
+++ src/screens/StartPage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const StartPage = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>abdasdasdasc</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default StartPage;
