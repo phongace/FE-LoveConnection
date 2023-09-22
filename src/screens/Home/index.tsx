fe6730a68ba9b5be9e11a26f3d371a314a42742d
Hoang Phong <phongnh2@smartosc.com>
2023-09-22 17:57:21 +0700

    [UI]: Topics slider

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,68 @@
-import React from 'react';
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  Animated,
+  FlatList,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
+import {
+  Directions,
+  FlingGestureHandler,
+  State,
+} from 'react-native-gesture-handler';
 import UserIcon from 'src/assets/icons/user.svg';
 import CardItem from './components/CardItem';
 import { styles } from './style';
 import useHome from './useHome';
 
+const TOPICS = [
+  {
+    title: 'Tình yêu',
+    total: '100',
+    url: require('src/assets/images/love-home.png'),
+  },
+  {
+    title: 'Gia đình',
+    total: '99',
+    url: require('src/assets/images/family-home.png'),
+  },
+  {
+    title: 'Bạn bè',
+    total: '98',
+    url: require('src/assets/images/friends-home.png'),
+  },
+  {
+    title: 'Đồng nghiệp',
+    total: '97',
+    url: require('src/assets/images/colleague-home.png'),
+  },
+];
+const VISIBLE_ITEMS = 3;
+
 const Home = () => {
   const { handleSession } = useHome();
+
+  const scrollXIndex = useRef(new Animated.Value(0)).current;
+  const scrollXAnimated = useRef(new Animated.Value(0)).current;
+
+  const [index, setIndex] = useState(0);
+  const setActiveIndex = useCallback(
+    (activeIndex: any) => {
+      scrollXIndex.setValue(activeIndex);
+      setIndex(activeIndex);
+    },
+    [scrollXIndex],
+  );
+
+  useEffect(() => {
+    Animated.spring(scrollXAnimated, {
+      toValue: scrollXIndex,
+      useNativeDriver: true,
+    }).start();
+  });
 
   const renderHeader = () => (
     <View style={styles.flexCenter}>
@@ -19,13 +74,76 @@ const Home = () => {
   );
 
   const renderCardItems = () => (
-    <>
-      <CardItem
-        title="Tình yêu"
-        totalQuestions="100"
-        imageUrl={require('src/assets/images/love-home.png')}
-      />
-    </>
+    <FlingGestureHandler
+      key="left"
+      direction={Directions.LEFT}
+      onHandlerStateChange={ev => {
+        if (ev.nativeEvent.state === State.END) {
+          if (index === TOPICS.length - 1) {
+            return;
+          }
+          setActiveIndex(index + 1);
+        }
+      }}>
+      <FlingGestureHandler
+        key="right"
+        direction={Directions.RIGHT}
+        onHandlerStateChange={ev => {
+          if (ev.nativeEvent.state === State.END) {
+            if (index === 0) {
+              return;
+            }
+            setActiveIndex(index - 1);
+          }
+        }}>
+        <FlatList
+          data={TOPICS}
+          keyExtractor={(_, index) => String(index)}
+          horizontal
+          inverted
+          contentContainerStyle={{
+            flex: 1,
+            justifyContent: 'center',
+            padding: 20,
+            height: 300,
+          }}
+          scrollEnabled={false}
+          removeClippedSubviews={false}
+          renderItem={({ item, index }) => {
+            const { title, total, url } = item;
+            const inputRange = [index - 1, index, index + 1];
+            const translateX = scrollXAnimated.interpolate({
+              inputRange,
+              outputRange: [50, 0, -100],
+            });
+            const scale = scrollXAnimated.interpolate({
+              inputRange,
+              outputRange: [0.8, 1, 1.3],
+            });
+            const opacity = scrollXAnimated.interpolate({
+              inputRange,
+              outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0],
+            });
+            return (
+              <Animated.View
+                style={{
+                  position: 'absolute',
+                  left: -10,
+                  opacity,
+                  transform: [
+                    {
+                      translateX,
+                    },
+                    { scale },
+                  ],
+                }}>
+                <CardItem title={title} totalQuestions={total} imageUrl={url} />
+              </Animated.View>
+            );
+          }}
+        />
+      </FlingGestureHandler>
+    </FlingGestureHandler>
   );
 
   return (
